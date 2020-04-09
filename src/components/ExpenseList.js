b6127de03bea7a3a7f23c51c170d9f34a0329b38
Duncan Moyo <duncanfmoyo@gmail.com>
2020-04-09 17:20:17 +0200

    adding buttons

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,20 +1,23 @@
 import React from "react";
 import ExpenseItem from "./ExpenseItem";
+import { MdDelete } from "react-icons/md";
 
-
-const ExpenseList = ({expenses}) => {
+const ExpenseList = ({ expenses }) => {
   return (
     <>
       <ul>
         {expenses.map((expense) => {
-          return <ExpenseItem key={expense.id} expense={expense}/>
+          return <ExpenseItem key={expense.id} expense={expense} />;
         })}
       </ul>
 
-      {expenses.length > 0 && <button className='btn'>Clear Expenses</button>}
+      {expenses.length > 0 && (
+        <button className="btn">
+          Clear Expenses <MdDelete className="btn-icon" />
+        </button>
+      )}
     </>
-  )
-
+  );
 };
 
 export default ExpenseList;
